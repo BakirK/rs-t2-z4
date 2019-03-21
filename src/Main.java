0b6dea87a3c821789c01a5042a603ac49fa2c64f
Bakir Karovic <bakir.karovic@gmail.com>
2019-03-22 00:03:24 +0100

    Dodano koristenje bibliotecne funkcije za pronalazak maksimalnog clana niza.

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,4 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
 
@@ -17,20 +17,32 @@ public class Main {
         Scanner ulaz = new Scanner(System.in);
         double n;
         while(true) {
-            System.out.println("Unesite broj elemenata niza: ");
+            System.out.print("Unesite broj elemenata niza: ");
             n = ulaz.nextDouble();
             if (n<1 || n > 100) {
-                System.out.println("Pogresan interval: ");
+                System.out.print("Pogresan interval: ");
                 continue;
             } else {
                 break;
             }
         }
 
-        System.out.println("Unesite elemente niza: ");
+        System.out.print("Unesite elemente niza: ");
         for (int i = 0; i < n; i++) {
             numbers[i] = ulaz.nextDouble();
         }
+        DoubleSummaryStatistics test = Arrays.stream(numbers).summaryStatistics();
+        double max = test.getMax();
+        System.out.println("Broj elemenata koji su veci od korjena najveceg elementa je: " + broj_Najvecih(numbers, Math.sqrt(max)));
+
+
+        /*
+        double max = Arrays.stream(numbers).min().getAsDouble();
+        System.out.println(max);
+        System.out.println(broj_Najvecih(numbers, Math.sqrt(max)));
+
+        List lista = Arrays.asList(numbers);
+        System.out.println(Collections.max(lista));*/
 
 
     }
